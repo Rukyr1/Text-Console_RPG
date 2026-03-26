db5a6d60a813a5572503f92116539b80c8a41bc0
leesungjin1024 <lsg21024@gmail.com>
2026-03-26 18:23:57 +0900

    🚚 move. Item,cpp 파일내용을  Inventory.h ,  Item.h 로 정리  🎉 add . Inventorty.h 생성   ✨ feat. Inventory.h  골드 기능 추가   📝 chore 메인에 주석 추가

--- Text-Console_RPG/Item.h
+++ Text-Console_RPG/Item.h
@@ -1,5 +1,27 @@
+//Item.h
 #pragma once
+
+#include <string>
+
 class Item
 {
-};
+public:
+	// 이름 가격 효과
+	Item(const std::string& name = "", int price = 0, int effect = 0);
 
+
+
+	const std::string& GetName() const;
+	int GetPrice() const;
+	int GetEffect() const;
+
+	void clear();
+
+	void Use() const;
+
+
+private:
+	std::string Iname;
+	int Iprice;
+	int Ieffect;
+};
--- Text-Console_RPG/main.cpp
+++ Text-Console_RPG/main.cpp
@@ -4,7 +4,8 @@
 #include<string>
 #include "Pokemon.h"
 #include "Bulbasaur.h"
-
+#include "Inventory.h"
+#include "Item.h"
 
 //포켓몬 타입 정의
 enum class  P_type //일단 스타팅 포켓몬 타입 3개만
@@ -16,12 +17,18 @@ enum class  P_type //일단 스타팅 포켓몬 타입 3개만
 
 int main()
 {
+	
 	//P_type my_type = P_type::GRASS or FIRE or WATER;
 	return 0;
 }
 
 
-// Inventory<Item> MyInven(120); //인벤토리 칸수
-// MyInven.Additem(Item("회복약", 10, 20)); //인벤에 회복약 넣기 //(이름 가격 효과)
-// MyInven.Printallitems(); // 가지고 있는 아wlrma이템 출력
-// MyInven.UseItem(1); // 회복약 사용
+//Inventory<Item> MyInven(10); //인벤토리 칸수 설정 // 인벤토리칸 필수 제일 위에 올라와야함
+//MyInven.Additem(Item("ex회복약", 10, 20)); //인벤에 ex회복약 넣기 //("이름": 가격: 효과:)
+//MyInven.UseItem(1); // ex회복약 사용 
+//MyInven.Additem(Item("ex회약", 10, 20));
+//MyInven.UseItem(2); // 회약 사용  // 번호순으로 저장
+//MyInven.Printallitems(); // 가지고 있는 아이템 출력
+//MyInven.AddGold(10);// 골드 획득 (10골드) // 골드 획득시 문구 출력
+//MyInven.UseGold(100);// 골드 사용 (100골드) // 골드 사용시 문구 출력 
+//MyInven.Printallgold();// 가지고 있는 골드 출력 
