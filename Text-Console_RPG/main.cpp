484e89d98d8453632e1a53dc9f45ee9d8a816cf9
leesungjin1024 <lsg21024@gmail.com>
2026-03-27 20:37:43 +0900

    [성진][Main.cpp] [Item.cpp]
    테스트 한거 지워서 다시푸시
    아이템 주석추가

--- Text-Console_RPG/main.cpp
+++ Text-Console_RPG/main.cpp
@@ -1,20 +1,10 @@
 //main.cpp
 #include "GameManager.h"
-#include "Inventory.h"
-#include "Item.h"
 
 
 int main()
 {
-	GameManager Stage;
-	Pokemon Stat;
-	Stage.GameStart();
-	
-	Inventory<Item> MyInven("마을사람1", 10);
-	MyInven.PrintStatus();
-	MyInven.Additem(Item("ex회복약", 11, 20));
-	MyInven.Additem(Item("ex회복약", 12, 20));
-	MyInven.PrintStatus();
+
 	//if (PCurrentHp <= 0){
 	//	Stage.GameEnding()
 	//		stat.printstatus() //현재 상태 출력
